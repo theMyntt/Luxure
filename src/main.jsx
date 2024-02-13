91dffef71881ee6677b3af16d30eb690219d1b6d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-13 00:26:46 -0300

    feat: err404 component

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import './index.css'
 import Masterpage from './pages/Masterpage'
+import Err404 from './pages/Err/404'
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Err404 />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Err/404.jsx
@@ -0,0 +1,10 @@
+import "./css/style.css"
+
+export default function Err404() {
+  return (
+    <div className="err404 err">
+      <h1>Erro 404</h1>
+      <p>Página não encontrada. <a href="/">Voltar para o início</a></p>
+    </div>
+  )
+}
