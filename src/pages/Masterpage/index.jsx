66c2c864ef4d04265e03a56be4fcf3f7658a4932
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-12 22:25:45 -0300

    feat: logo and nav in masterpage

---
+++ src/pages/Masterpage/index.jsx
@@ -0,0 +1,17 @@
+import Logo from "../../assets/icon/logo.png";
+import "./css/style.css"
+
+export default function Masterpage() {
+  return (
+    <div className="masterpage">
+      <img src={Logo} alt="logo" id="logo"/>
+      <div className="nav">
+        <ul>
+          <li><a href="#home">Masculina</a></li>
+          <li><a href="#about">Feminina</a></li>
+          <li><a href="#contact">Kids</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
