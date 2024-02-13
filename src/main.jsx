99673c5b2d59d7ee05aef9e87adb7aecea468a2f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-12 22:07:25 -0300

    feat: router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 import './index.css'
+import Masterpage from './pages/Masterpage'
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Masterpage />
+    <Router />
   </React.StrictMode>,
 )
